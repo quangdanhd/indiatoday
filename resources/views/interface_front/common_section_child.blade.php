3b10a0794d0adfdace8348f8f38fbb1e38ff81e9
quangdanhd <quangdan.hd@gmail.com>
2021-08-11 10:05:12 +0700

    Fix home category title

--- resources/views/interface_front/common_section_child.blade.php
+++ resources/views/interface_front/common_section_child.blade.php
@@ -5,7 +5,7 @@
             <div class="droppable ">
                 <div class="widget-wrapper section_wise_order">
                 <span class="widget-title">
-                    <a href="">{{strtolower($value[0]['name'])}}</a>
+                    <a href="">{{$value[0]['name']}}</a>
                 </span>
                     <div class="data-holder" id="itg-block-{{$loop->index + 7}}">
                         <div class="section-ordering video-gif">
