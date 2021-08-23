ff248de6dde54793e7822a4561d1c97bdcc56072
quangdanhd <quangdan.hd@gmail.com>
2021-08-23 10:28:15 +0700

    Cached access times

--- app/Http/Controllers/ControllerUsers.php
+++ app/Http/Controllers/ControllerUsers.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class ControllerUsers extends BaseController
@@ -16,11 +17,19 @@ class ControllerUsers extends BaseController
     public function __construct()
     {
         $uniqueID = session_uniqueID();
-        $access_today = DB::table('access_times')->select('id')->whereDate('created_at', date('Y-m-d'))->where('unknown_token', $uniqueID)->first();
-        if (!$access_today) {
-            access_times::create([
-                'unknown_token' => $uniqueID,
-            ]);
+        $cache_key = 'access_times_' . $uniqueID;
+        $access_times = Cache::get($cache_key);
+        // Check cached
+        if ($access_times != date('Y-m-d')) {
+            $access_today = DB::table('access_times')->select('id')->whereDate('created_at', date('Y-m-d'))->where('unknown_token', $uniqueID)->first();
+            // Check if cache cleared
+            if (!$access_today) {
+                access_times::create([
+                    'unknown_token' => $uniqueID,
+                ]);
+            }
+            $seconds = 24 * 60 * 60;
+            Cache::add($cache_key, date('Y-m-d'), $seconds);
         }
     }
 }
