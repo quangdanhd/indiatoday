cdc1f06c6a7db31bd00826b29c61b16271a9cd2d
dandqgravitassoft <dandq@gravitassoft.com>
2021-09-01 23:34:33 +0700

    feed update

--- resources/views/rss/feed.blade.php
+++ resources/views/rss/feed.blade.php
@@ -1,20 +1,19 @@
 <?=
 '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL
 ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
     <channel>
         <title>Mekong24h</title>
         <link>https://mekong24h.xyz</link>
         <description>Check out the latest news around the world.</description>
         <language>en-us</language>
-        <pubDate>{{ now() }}</pubDate>
+        <pubDate>{{ now()->format(DateTime::RFC822) }}</pubDate>
         @foreach($posts as $post)<item>
             <title>{{ $post->title }}</title>
             <link>https://mekong24h.xyz/{{ $post->url }}</link>
             <description>{{ $post->describe }}</description>
             <category>{{ $post->name }}</category>
-            <author>admin</author>
-            <guid>{{ $post->id }}</guid>
+            <guid isPermaLink="true">https://mekong24h.xyz/{{ $post->url }}</guid>
             <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
         </item>
         @endforeach
