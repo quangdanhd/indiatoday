b4949a490ed4572e35242fd95f26412e08fdceb0
dandqgravitassoft <dandq@gravitassoft.com>
2021-09-01 23:16:34 +0700

    update feed

--- resources/views/rss/feed.blade.php
+++ resources/views/rss/feed.blade.php
@@ -3,22 +3,20 @@
 ?>
 <rss version="2.0">
     <channel>
-        <title><![CDATA[ Mekong24h ]]></title>
-        <link>
-        <![CDATA[ https://mekong24h.xyz/feed ]]></link>
-        <description><![CDATA[ Check out the latest news around the world. ]]></description>
-        <language>vi</language>
+        <title>Mekong24h</title>
+        <link>https://mekong24h.xyz</link>
+        <description>Check out the latest news around the world.</description>
+        <language>en-us</language>
         <pubDate>{{ now() }}</pubDate>
-        @foreach($posts as $post)
-            <item>
-                <title><![CDATA[{{ $post->title }}]]></title>
-                <link>https://mekong24h.xyz/{{ $post->url }}</link>
-                <description><![CDATA[{!! $post->describe !!}]]></description>
-                <category>{{ $post->name }}</category>
-                <author><![CDATA[ admin ]]></author>
-                <guid>{{ $post->id }}</guid>
-                <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
-            </item>
+        @foreach($posts as $post)<item>
+            <title>{{ $post->title }}</title>
+            <link>https://mekong24h.xyz/{{ $post->url }}</link>
+            <description>{{ $post->describe }}</description>
+            <category>{{ $post->name }}</category>
+            <author>admin</author>
+            <guid>{{ $post->id }}</guid>
+            <pubDate>{{ $post->created_at->toRssString() }}</pubDate>
+        </item>
         @endforeach
     </channel>
 </rss>
